e3d78ce65c0d42e30f40c6dd21a702f227d4524b
Gergo Fazekas <fazekasgergo225@gmail.com>
2024-09-07 12:22:01 +0300

    Initial Commit

---
+++ EmojiCam/EmojiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCamApp.swift
+//  EmojiCam
+//
+//  Created by Fazekas, Gergo on 07.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
